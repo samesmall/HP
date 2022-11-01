192658002ec869b4b5f82b5bec400bdb18f4f9ce
amnads <amnads@imt-soft.com>
2022-11-01 10:44:28 +0700

    background

--- core/src/com/mygdx/game/hiccupyperry.java
+++ core/src/com/mygdx/game/hiccupyperry.java
@@ -12,7 +12,7 @@ public class hiccupyperry extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("logo.png");
 	}
 
 	@Override
